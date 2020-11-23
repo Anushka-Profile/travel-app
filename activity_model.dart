45ac17465e526a44ff83cf3fd125a421e9d9f064
AnushkaDarade <54483295+smart-girll@users.noreply.github.com>
2020-11-23 21:07:07 +0530

    in lib sub folder model

---
+++ activity_model.dart
@@ -0,0 +1,15 @@
+class Activity {
+  String imageURL;
+  String name;
+  String type;
+  List<String> startTimes;
+  int price;
+
+  Activity({
+    this.imageURL,
+    this.name,
+    this.type,
+    this.startTimes,
+    this.price,
+  });
+}
---
+++ country_model.dart
@@ -0,0 +1,8 @@
+class CountryModel {
+  String label;
+  String countryName;
+  int noOfTours;
+  double rating;
+  String imgURL;
+  CountryModel({this.countryName, this.label, this.noOfTours, this.rating, this.imgURL});
+}
