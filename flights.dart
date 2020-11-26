8411088b2fe1538d83d3d7abde08fe26cf6b85fc
AnushkaDarade <54483295+smart-girll@users.noreply.github.com>
2020-11-26 21:37:22 +0530

    Add files via upload

--- flights.dart
+++ flights.dart
@@ -2,6 +2,7 @@ import 'package:flutter1/model/flights_data.dart';
 import 'package:flutter1/model/activity_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter1/views/flight_booking.dart';
 
 class DestinationScreen extends StatefulWidget {
   @override
@@ -66,111 +67,122 @@ class _DestinationScreenState extends State<DestinationScreen> {
               itemCount: activities.length,
               itemBuilder: (BuildContext context, int index) {
                 Activity activity = activities[index];
-                return Stack(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
-                      height: 170.0,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0),
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => FlightScreen(
+                        activity: activity,
                       ),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Container(
-                                  width: 120.0,
-                                  child: Text(
-                                    '${activity.dept_name} -> ${activity.dest_name}',
-                                    style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                  ),
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text(
-                                      '\$${activity.price}',
+                    ),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                        height: 170.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    width: 120.0,
+                                    child: Text(
+                                      '${activity.deptName} -> ${activity.destName}',
                                       style: TextStyle(
-                                        fontSize: 22.0,
+                                        fontSize: 18.0,
                                         fontWeight: FontWeight.w600,
                                       ),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
                                     ),
-                                    Text(
-                                      'per pax',
-                                      style: TextStyle(
-                                        color: Colors.grey,
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Text(
+                                        '\$${activity.price}',
+                                        style: TextStyle(
+                                          fontSize: 22.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Text(
-                              activity.type,
-                              style: TextStyle(
-                                color: Colors.grey,
+                                      Text(
+                                        'per pax',
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(5.0),
-                                  width: 140.0,
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).accentColor,
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    '${activity.startTimes[0]} : ${activity.startTimes[1]}',
-                                  ),
+                              Text(
+                                activity.type,
+                                style: TextStyle(
+                                  color: Colors.grey,
                                 ),
-                                // SizedBox(width: 10.0),
-                                // Container(
-                                //   padding: EdgeInsets.all(5.0),
-                                //   width: 70.0,
-                                //   decoration: BoxDecoration(
-                                //     color: Theme.of(context).accentColor,
-                                //     borderRadius: BorderRadius.circular(10.0),
-                                //   ),
-                                //   alignment: Alignment.center,
-                                //   child: Text(
-                                //     activity.startTimes[1],
-                                //   ),
-                                // ),
-                              ],
-                            )
-                          ],
+                              ),
+                              SizedBox(height: 10.0),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.all(5.0),
+                                    width: 140.0,
+                                    decoration: BoxDecoration(
+                                      color: Theme.of(context).accentColor,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      '${activity.startTimes[0]} : ${activity.startTimes[1]}',
+                                    ),
+                                  ),
+                                  // SizedBox(width: 10.0),
+                                  // Container(
+                                  //   padding: EdgeInsets.all(5.0),
+                                  //   width: 70.0,
+                                  //   decoration: BoxDecoration(
+                                  //     color: Theme.of(context).accentColor,
+                                  //     borderRadius: BorderRadius.circular(10.0),
+                                  //   ),
+                                  //   alignment: Alignment.center,
+                                  //   child: Text(
+                                  //     activity.startTimes[1],
+                                  //   ),
+                                  // ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      left: 20.0,
-                      top: 15.0,
-                      bottom: 15.0,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: CachedNetworkImage(
-                          imageUrl: activity.imageURL,
-                          width: 110.0,
-                          fit: BoxFit.cover,
+                      Positioned(
+                        left: 20.0,
+                        top: 15.0,
+                        bottom: 15.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: CachedNetworkImage(
+                            imageUrl: activity.imageURL,
+                            width: 110.0,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 );
               },
             ),
